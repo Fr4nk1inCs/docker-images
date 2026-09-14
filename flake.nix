ab9a65a673562e4be58d9d3c59a9e5bb9d13853e
Fr4nk1in <fushen@mail.ustc.edu.cn>
2026-09-15 03:27:15 +0800

    Add Nix devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "docker-images";
+
+            packages = with pkgs; [
+              actionlint
+              shellcheck
+            ];
+          };
+        };
+    };
+}
